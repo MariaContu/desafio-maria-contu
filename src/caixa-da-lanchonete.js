cfa792db2839e9fc9bb92dbb49f9392c7d8308e6
MariaContu <mariaescontu@gmail.com>
2023-08-11 17:49:14 -0300

    getts e refatoracao

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,3 +1,6 @@
+import { Item } from "./Models/item";
+import { FormasPagamento } from "./Models/formasPagamento.js";
+
 class CaixaDaLanchonete {
 
     calcularValorDaCompra(metodoDePagamento, itens) {
